cc433647b680e2ef67381120ed9bb16a07947ea5
sathish144625 <sathishkumar144625@gmail.com>
2022-04-27 16:54:24 +0530

    maven
    Hello word Example

---
+++ Maven/xtendo-login/src/main/java/com/xtendo/auth/App.java
@@ -0,0 +1,18 @@
+package com.xtendo.auth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+System.out.println( "Hello World!" );
+System.out.println( "Hello World!" );
+System.out.println( "Hello World!" );
+System.out.println( "Hello World!" );
+System.out.println( "Hello World!" );
+    }
+}
